a20301759eb40958d3c5a6f925bccafd57f8a157
nextechnexus <nextechnexus.team@gmail.com>
2024-01-15 23:14:02 +0100

    commit

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,8 +1,11 @@
 const asyncHandler = require("express-async-handler")
+
+const Goal = require("../model/goalModel")
 // @ desc    Get goals
 // @ route    Get /api/goal
 const getGoal  = asyncHandler(async(req,res)=>{
-    res.status(200).json({message:'Get Goals'})
+    const goal = await Goal.find()
+    res.status(200).json(goal)
 })
 
 const setGoal  = asyncHandler(async(req,res)=>{
@@ -11,19 +14,36 @@ const setGoal  = asyncHandler(async(req,res)=>{
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message:'Get Goals'})
+    const goal = await Goal.create(
+        {
+            text:req.body.text
+        }
+    )
+
+    res.status(200).json(goal)
 })
 
 const updateGoal  = asyncHandler(async(req,res)=>{
-    res.json({
-        message:`Update Goals ${req.params.id}`
-    })
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal Not Found")
+    }
+
+    const updateGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{new:true})
+
+    res.json(updateGoal)
 })
 
 const deleteGoal  = asyncHandler(async(req,res)=>{
-    res.json({
-        message:`Delete Goals ${req.params.id}`
-    })
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal Not Found")
+    }
+    await goal.remove()
+    res.json({id:req.params.id})
 })
 
 module.exports = {
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const colors = require("colors")
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+
+connectDB()
 const port  = process.env.PORT||5000
 const {errorHandler} = require('./middleware/middelware')
 
